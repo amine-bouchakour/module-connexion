cbf10081cdc50201680476265decb40e511e4280
Bouchakour <amine.bouchakour@laplateforme.io>
2019-11-13 23:05:34 +0100

    Avancements page profil

--- profil.php
+++ profil.php
@@ -11,12 +11,36 @@ qui sont actuellement stockées en base de données. -->
 
 <?php
 session_start();
+session_destroy();
+
+session_start();
+$_SESSION ['prenom']=$_POST['prenom'];
+$_SESSION ['nom']=$_POST['nom'];
+$_SESSION ['login']= $_POST['login'];
+$_SESSION ['password']=$_POST['password'];
+$_SESSION ['confirmpassword']=$_POST['confirmpassword'];
+
+
+
+
 
 //Requete sur TOUTES les infos
 $connexion = mysqli_connect("localhost","root","","moduleconnexion");
 $requete = "SELECT `login`,`prenom`,`nom`,`password` FROM `utilisateurs` ORDER BY `utilisateurs`.`Id` ASC";
 $query = mysqli_query($connexion,$requete);
 $resultat= mysqli_fetch_all($query);
+
+
+$connexion1 = mysqli_connect("localhost","root","","moduleconnexion");
+$requete1 = "UPDATE utilisateurs
+SET login = '$_POST[login]',
+  prenom = '$_POST[prenom]',
+  nom = '$_POST[nom]',
+  password = '$_POST[password]'
+WHERE id = 175";
+$query1 = mysqli_query($connexion1,$requete1);
+$resultat1= mysqli_fetch_all($query1);
+
 
 
 
@@ -28,19 +52,21 @@ $resultat= mysqli_fetch_all($query);
 
 <h1> Page Profil </h1>
 
-<form action="connexion.php" name="modification" method="post">
+<form action="profil.php" name="modification" method="post">
 
-<label for="login">Login : </label> <input type="text" name="login" value="<?php echo $resultat[0][0] ; ?>" required><br>
-<label for="prenom">Prénom : </label> <input type="text" name="prenom" value="<?php echo $resultat[0][2] ; ?>"  required><br>
-<label for="nom">Nom : </label> <input type="text" name="nom" value="<?php echo $resultat[0][2] ; ?>"><br>
-<label for="password">Password : </label> <input type="text" name="password" value=" <?php echo $resultat[0][3] ; ?>"  required><br>
-<label for="confirmpassword">Confirmation password :</label>  <input type="text" name="confirmpassword" value="<?php echo $resultat[0][3]; ?>" required><br>
+<label for="login">Login : </label> <input type="text" name="login" placeholder="<?php echo $resultat[1][0] ; ?>" value="" required><br>
+<label for="prenom">Prénom : </label> <input type="text" name="prenom" placeholder="<?php echo $resultat[1][1] ; ?>" value=""  required><br>
+<label for="nom">Nom : </label> <input type="text" name="nom" placeholder="<?php echo $resultat[1][2] ; ?>" value=""><br>
+<label for="password">Password : </label> <input type="text" name="password" placeholder=" <?php echo $resultat[1][3] ; ?>" value=""  required><br>
+<label for="confirmpassword">Confirmation password :</label>  <input type="text" name="confirmpassword" placeholder="<?php echo $resultat[1][3]; ?>" value="" required><br>
 <input type="submit" name="submit" value="Modifier"><br>
 </form>
 
+
+
 <?php
 
-
+echo 'Bienvenue à toi '.$_SESSION['login'].' '.$_SESSION['prenom'].'<br/>';
 
 ?>
 
