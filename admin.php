6ffc3e2c67715ab7127b92ead59f86d02bbb9075
Bouchakour <amine.bouchakour@laplateforme.io>
2019-11-15 23:47:35 +0100

    Pages avancées

--- admin.php
+++ admin.php
@@ -1,15 +1,41 @@
 <html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Page Administrateur</title>
+    <link rel="stylesheet" href="moduleconnexion1.css">
+</head>
+
 <h1> Page Administrateur </h1>
 
             <!-- PHP -->
 <?php
 session_start();
 
-//Requete sur TOUTES les infos des utilisateurs
+function affichagetoutesdonnees()
+{
+    //Requete sur TOUTES les infos des utilisateurs
 $connexion = mysqli_connect("localhost","root","","moduleconnexion");
 $requete = "SELECT * FROM `utilisateurs` ORDER BY `utilisateurs`.`Id` ASC";
 $query = mysqli_query($connexion,$requete);
 $resultat= mysqli_fetch_all($query);
+
+    $i=0;
+    while($i<count($resultat))
+        {
+            $j=0;
+            echo "<tr>";
+
+            while ($j<count($resultat[$i]))
+                {
+                    echo "<td>".$resultat[$i][$j]."</td>";
+                    $j++;
+                }
+                
+            echo "</tr>";
+            $i++;
+        } 
+}
 
 ?>
 
@@ -19,7 +45,7 @@ $resultat= mysqli_fetch_all($query);
 <!-- TABLEAU AVEC AFFICHAGE REQUETTE MySQL TOUTES LES INFORMATIONS SUR TOUT LES UTILISATEURS DANS LA BDD MySQL -->
 <table>
     <thead>
-        <tr>
+        <tr class="tr">
             <th>Id</th>
             <th>Login</th>
             <th>Prénom</th>
@@ -28,24 +54,9 @@ $resultat= mysqli_fetch_all($query);
         </tr>
     </thead>
     <tbody>
-        
-            <?php 
-            $i=0;
-            while($i<count($resultat))
-                {
-                    $j=0;
-                    echo "<tr>";
-
-                    while ($j<count($resultat[$i]))
-                        {
-                            echo "<td>".$resultat[$i][$j]."</td>";
-                            $j++;
-                        }
-                        
-                    echo "</tr>";
-                    $i++;
-                } 
-            ?>
+        <?php 
+            affichagetoutesdonnees()
+        ?>
 
     </tbody>
 </table>
--- index.php
+++ index.php
@@ -3,65 +3,103 @@
 <!doctype html>
 <html lang="fr">
 
-<head>
-    <meta charset="utf-8">
-    <title>Page d'acceuil</title>
-    <link rel="stylesheet" href="moduleconnexion.css">
+    <head>
+        <meta charset="utf-8">
+        <title>Page d'acceuil</title>
+        <link rel="stylesheet" href="moduleconnexion.css">
+        
+    </head>
+
+    <body>
+
+        <header>
+
+
+        </header>
+
+            <form action="" method="post">
+            
+            <input type="submit" name="seconnecter" value="Se connecter">
+            <input type="submit" name="sinscrire" value="S'inscrire">
+            <input type="submit" name="modifierprofil" value="modifier profil">
+            <input type="submit" name="deconnexion" value="Deconnexion">
+            </form>
+        
+        <main>
+
+            <?php 
+            if($_SESSION['login'])
+
+            {
+                
+                {
+                    echo '<h1>'.'Bienvenue sur notre site '.$_SESSION['login'].' !'.'</h1>'.'<br/>';
+                }
+
+                if(isset($_POST['deconnexion']))
+                    {
+                        session_start();
+                        session_destroy();
+                        header ('Location:index.php');
+                    }
+
+                
+
+                if(isset($_POST['modifierprofil']))
+                    {
+                        header ('Location:profil.php');
+                    }
+            }
+
+
+
+            else 
+
+            {
+                
+                if(isset($_POST['seconnecter']))
+                    {
+                        header ('Location:connexion.php');
+                    }
+                
+                echo '<h1>'.'Bienvenue sur notre site nouvel aventurier !'.'</h1>';
+
+                if(isset($_POST['sinscrire']))
+                    {
+                        header ('Location:inscription.php');
+                    }
+
+            }
+            ?>
     
-</head>
-
-<body>
-
-    <header>
-
-
-    </header>
-    <form action="" method="post">
-<input type="submit" name="deconnexion" value="deconnexion">
-    
-</form>
-    <main>
-
-        <?php 
-        
-        if($_SESSION['login'])
-
-        {
-            echo '<h1>'.'Bienvenue sur notre site '.$_SESSION['login'].' !'.'</h1>'.'<br/>';
-        }
-
-        
-        
-        if(isset($_POST['deconnexion']))
-        {
-            session_start();
-            session_destroy();
-            header ('Location:connexion.php');
-        }
-        
-        ?>
+            
+            
 
 
 
 
 
-    </main>
+        </main>
 
-    <footer>
+        <footer>
 
-    </footer>
+        </footer>
 
-</body>
+    </body>
 
 
 
 
 </html>
 
-<input type="submit" name="modifierprofil" value="modifier profil"></div>
 
-// CONDITION ET LIEN POUR MODIFIER PROFIL
-    if(isset($_POST['modifierprofil'])==true)
-    {
-        header ('Location:profil.php');
-    }
+
+if(isset($_POST['seconnecter']))
+                    {
+                        header ('Location:connexion.php');
+                    }
+
+                if(isset($_POST['sinscrire']))
+                    {
+                        header ('Location:inscription.php');
+                    }
